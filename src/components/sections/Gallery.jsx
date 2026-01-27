c8a6f31506c1732c8a1f0369ae41e13a23370439
Jasper Tabin <tabinjasper@gmail.com>
2026-01-28 04:25:30 +0800

    Redo of the file path

--- src/components/sections/Gallery.jsx
+++ src/components/sections/Gallery.jsx
@@ -2,17 +2,16 @@ import { useState, useRef, useEffect } from "react";
 import { ChevronLeft, ChevronRight, Images } from "lucide-react";
 
 const images = [
-  { id: 1, src: "/public/Gallery/G1.jpg", alt: "Photo 1" },
-  { id: 2, src: "/public/Gallery/G2.jpg", alt: "Photo 2" },
-  { id: 3, src: "/public/Gallery/G3.jpg", alt: "Photo 3" },
-  { id: 4, src: "/public/Gallery/G4.jpg", alt: "Photo 4" },
-  { id: 5, src: "/public/Gallery/G5.jpg", alt: "Photo 5" },
-  { id: 6, src: "/public/Gallery/G6.jpg", alt: "Photo 6" },
-  { id: 7, src: "/public/Gallery/G7.jpg", alt: "Photo 7" },
-  { id: 8, src: "/public/Gallery/G8.jpg", alt: "Photo 8" },
-  { id: 9, src: "/public/Gallery/G9.jpg", alt: "Photo 9" },
+  { id: 1, src: "/Gallery/G1.jpg", alt: "Photo 1" },
+  { id: 2, src: "/Gallery/G2.jpg", alt: "Photo 2" },
+  { id: 3, src: "/Gallery/G3.jpg", alt: "Photo 3" },
+  { id: 4, src: "/Gallery/G4.jpg", alt: "Photo 4" },
+  { id: 5, src: "/Gallery/G5.jpg", alt: "Photo 5" },
+  { id: 6, src: "/Gallery/G6.jpg", alt: "Photo 6" },
+  { id: 7, src: "/Gallery/G7.jpg", alt: "Photo 7" },
+  { id: 8, src: "/Gallery/G8.jpg", alt: "Photo 8" },
+  { id: 9, src: "/Gallery/G9.jpg", alt: "Photo 9" },
 ];
-
 export const Gallery = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const viewportRef = useRef(null);
